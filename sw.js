1a7e4d37a77a262e0ec93a6994f8bec231b3a220
dvilrgamerz <chetnapatel084@gmail.com>
2026-09-10 11:24:28 -0500

    Add offline app shell service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'wifi-pulse-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))));
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))));
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin || event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    const copy = response.clone(); caches.open(CACHE).then((c) => c.put(event.request, copy)); return response;
+  })));
+});
